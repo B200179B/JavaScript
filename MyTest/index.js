356fc9af762bf209009b692f7519426a2082a828
ChuaCL <B200179B@sc.edu.my>
2020-12-11 15:03:19 +0800

    1

---
+++ MyTest/index.js
@@ -0,0 +1,124 @@
+// var width = 20;                 //Number
+// var Name = "Ali";                   //String
+// var Name2 = 'Abu';                  //String
+// var x = {firstName:"John", lastName:"Snow"};        //Object
+// var a = ["John","Cersei","Stark"];              //Array
+// var malaysia;
+
+// malaysia = "Malaysia";
+
+// console.log (width + Name + Name2);
+
+// console.log (x);
+
+// console.log (malaysia);
+
+// console.log (a);
+
+// malaysia = 13;
+
+// console.log (malaysia);
+
+
+
+
+// var i = 10;
+
+// if(i === 10){
+//     console.log("i is more than 10");
+// }else if(i == "10"){
+//     console.log("i is equal to string ten")
+// }else{
+//     console.log("i is less than 10");
+// }
+
+
+
+
+// var i = "10";
+
+// if(i === 10){
+//     console.log("i is more than 10");
+// }else if(i == "10"){
+//     console.log("i is equal to string ten")
+// }else{
+//     console.log("i is less than 10");
+// }
+
+
+
+
+// exercise 1
+// for(var i=0;i<3;i++){
+//     console.log(i);
+// }
+
+// for(var y=5 ; y>0 ; y--){
+//     console.log("y is = "+y);
+// }
+
+// for(var i=0 ; i < 100 ; i+=5){
+//     console.log(i);
+// }
+
+// for(var y = 50 ; y>0 ; y-=2){
+//     console.log("y is = "+y);
+// }
+
+
+
+
+
+// var i =0;
+// var test="";
+
+// while (i<10){
+//     text += "The number is" + i;
+//     i++;
+//     console.log(text);
+
+
+
+
+// function addValue(a,b){
+//     console.log(a+b);
+// }
+// addValue(2,4);
+
+
+
+
+
+// function calculatePuppyAge(age,sex){
+//     var puppyAge = 7*age;
+//     console.log("Your doggie age is " + puppyAge+ " compare to human");
+// }
+// calculatePuppyAge(2,", male");
+// calculatePuppyAge(10,",female");
+
+
+
+
+
+
+
+
+
+
+
+// document.body.style.background="red";
+// var p = document.createElement("P");
+// document.body.appendChild(p);
+// p.innerHTML = "This is a paragraph";
+
+// document.getElementById("blue").style.color="blue";
+// document.getElementsByClassName("test")[0].style.color="yellow";
+
+
+// function makeBlue(){
+//     document.body.style.background="blue";
+// }
+
+// function makeWhite(){
+//     document.body.style.background+"white";
+// }
