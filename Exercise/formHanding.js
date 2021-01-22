2ac9914a232ff8001b7a78454e8dee51a1754e57
B200179B <b200179b@sc.edu.my>
2021-01-22 15:03:06 +0800

    ccc

---
+++ Exercise/formHanding.js
@@ -0,0 +1,14 @@
+// function ageFunction(){
+//     var x,text;
+//     x= document.getElementById("age").value;
+//     if(x<18){
+//         alert("age is less than 18. You are not an adult");
+//     }
+// }
+// function nameFunction(){
+//     var y,text;
+//     y=document.getElementById("name").value;
+//     if(y==""){
+//         alert("Please fill in Name");
+//     }
+// }
